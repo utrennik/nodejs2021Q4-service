9ddd46f1db263dce74b348293bc39b8da9030684
utrennik <utrennik@list.ru>
2022-01-12 13:25:45 +0300

    feat: implement JWT verify and payload check

--- src/auth/check-jwt.ts
+++ src/auth/check-jwt.ts
@@ -1,5 +1,8 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import ClientError from '../errors/client-error';
+import jwt from 'jsonwebtoken';
+import usersRepo from '../resources/users/users-repo';
+import { IAuthTokenPayload } from './types';
 import config from '../common/config';
 
 const checkJWT = async (req: FastifyRequest, res: FastifyReply) => {
@@ -10,7 +13,32 @@ const checkJWT = async (req: FastifyRequest, res: FastifyReply) => {
   if (authHeader && splitted && splitted[0] === 'Bearer' && splitted[1]) {
     const token = splitted[1];
 
-    console.log(`Token: ${token}`);
+    const payload = await new Promise((res, rej) => {
+      jwt.verify(token, config.JWT_SECRET_KEY, (err, payload) => {
+        if (err) rej(err);
+        res(payload);
+      });
+    }).catch((e) => {
+      throw new ClientError(e.message, config.HTTP_CODES.UNAUTHORIZED);
+    });
+
+    console.log(`Token payload: ${JSON.stringify(payload)}`);
+
+    const { userId, login } = payload as IAuthTokenPayload;
+
+    const userById = await usersRepo.getUserByID(userId);
+    if (!userById)
+      throw new ClientError(
+        'Userid defined in Authorization token not found',
+        config.HTTP_CODES.UNAUTHORIZED
+      );
+
+    const userByLogin = await usersRepo.getUserByLogin(login as string);
+    if (!userByLogin)
+      throw new ClientError(
+        'Username defined in the authorization token not found',
+        config.HTTP_CODES.UNAUTHORIZED
+      );
 
     return { req, res };
   }
---
+++ src/auth/types.ts
@@ -0,0 +1,4 @@
+export interface IAuthTokenPayload {
+  userId: string;
+  login: string;
+}
