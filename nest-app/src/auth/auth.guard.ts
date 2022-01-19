c77bc31eb7f55ff5b852ac52af2e36e7d3ecf22b
utrennik <utrennik@list.ru>
2022-01-19 17:54:15 +0300

    feat: implement JWT authentication

--- nest-app/src/auth/auth.guard.ts
+++ nest-app/src/auth/auth.guard.ts
@@ -1,14 +1,48 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import jwt from 'jsonwebtoken';
+import config from '../common/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    return this.validateRequest(request);
-  }
 
-  async validateRequest(request: Request) {
-    console.log('validating request..');
+    const authHeader = request.headers.authorization;
+
+    const splittedHeader = authHeader ? authHeader.split(' ') : null;
+
+    if (
+      !(
+        authHeader &&
+        splittedHeader &&
+        splittedHeader[0] === 'Bearer' &&
+        splittedHeader[1]
+      )
+    ) {
+      throw new UnauthorizedException();
+    }
+
+    const token = splittedHeader[1];
+
+    const tokenPayload = await new Promise((resolve, reject) => {
+      jwt.verify(token, config.JWT_SECRET_KEY, (err, payload) => {
+        if (err) reject(err);
+
+        resolve(payload);
+      });
+    }).catch((e) => {
+      throw new UnauthorizedException(e.message);
+    });
+
+    console.warn(
+      `FOR CROSSCHECK! Token payload: ${JSON.stringify(tokenPayload)}`,
+    );
+
     return true;
   }
 }
