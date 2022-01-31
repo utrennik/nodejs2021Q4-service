83adf59b9bbae4ca95b93fc7fa35f26720b41401
Utrennik <utrennik@list.ru>
2022-01-31 12:39:57 +0300

    feat: add use-fastify support

--- src/common/config.ts
+++ src/common/config.ts
@@ -41,6 +41,7 @@ const config = {
   APP_HOST: process.env.APP_HOST || '0.0.0.0',
   STATIC_FILES_DIR: process.env.STATIC_FILES_DIR || './',
   CRYPT_SALT: 10,
+  USE_FASTIFY: process.env.USE_FASTIFY,
 };
 
 export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
 import { WinstonModule } from 'nest-winston';
 import { NestFactory } from '@nestjs/core';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import loggerSettings from './logger/logger.settings';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from './app.module';
 import config from './common/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger(loggerSettings),
-  });
+  let app: NestFastifyApplication | INestApplication;
+
+  if (config.USE_FASTIFY) {
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+      {
+        logger: WinstonModule.createLogger(loggerSettings),
+      },
+    );
+  } else {
+    app = await NestFactory.create<INestApplication>(AppModule, {
+      logger: WinstonModule.createLogger(loggerSettings),
+    });
+  }
 
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(config.PORT, config.APP_HOST);
