0fb772fbf7ed5b1d8f186c65552ca67140b38c10
Utrennik <utrennik@list.ru>
2022-02-05 12:25:15 +0300

    fix: fix logger middleware for express mode

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -3,6 +3,7 @@
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import * as urlParser from 'url';
+import config from '../common/config';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -25,16 +26,25 @@ export class LoggerMiddleware implements NestMiddleware {
   }
 
   use(req: Request, res: Response, next: NextFunction) {
-    let bodyStr = '';
-
     const isFile =
       req.headers['content-type'] &&
       req.headers['content-type'].includes('multipart/form-data');
+    const fileDetectedMessage = 'Detected content-type: multipart/form-data';
 
-    if (!isFile) {
-      req.on('data', (chunk) => {
-        bodyStr += chunk;
-      });
+    const isFastify = config.USE_FASTIFY === 'true';
+
+    let body = isFile ? fileDetectedMessage : '{}';
+
+    let bodyStr = '';
+
+    if (isFastify) {
+      if (!isFile) {
+        req.on('data', (chunk) => {
+          bodyStr += chunk;
+        });
+      }
+    } else {
+      body = JSON.stringify(req.body);
     }
 
     res.on('close', () => {
@@ -48,9 +58,7 @@ export class LoggerMiddleware implements NestMiddleware {
       const reqInfo = `Received request from URL: ${hostname}${url}, query params: ${JSON.stringify(
         query,
       )}, body: ${
-        bodyStr
-          ? JSON.stringify(JSON.parse(bodyStr))
-          : 'Detected content-type: multipart/form-data'
+        bodyStr ? JSON.stringify(JSON.parse(bodyStr)) : body
       }. Request completed with status code: ${statusCode}`;
 
       const firstDigit = `${statusCode}`[0];
